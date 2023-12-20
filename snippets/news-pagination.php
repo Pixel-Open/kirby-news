cbde3ab8757ab5f7c99aac2194b8c377e99f2c79
hugo <hugo.mercat@epita.fr>
2023-12-20 11:10:04 +0100

    rename class for 'pixel' instead of 'kirby'

--- snippets/news-pagination.php
+++ snippets/news-pagination.php
@@ -1,6 +1,6 @@
 <?php if ($pagination->hasPages()): ?>
-<nav class="pagination">
-  <a class="pagination__prev" <?php if ($pagination->hasPrevPage()): ?>href="<?= $pagination->prevPageUrl() ?>"<?php endif ?>>&larr;</a>
-  <a class="pagination__next" <?php if ($pagination->hasNextPage()): ?>href="<?= $pagination->nextPageUrl() ?>"<?php endif ?>>&rarr;</a>
+<nav class="news__pagination">
+  <a class="news__pagination__prev" <?php if ($pagination->hasPrevPage()): ?>href="<?= $pagination->prevPageUrl() ?>"<?php endif ?>>&larr;</a>
+  <a class="news__pagination__next" <?php if ($pagination->hasNextPage()): ?>href="<?= $pagination->nextPageUrl() ?>"<?php endif ?>>&rarr;</a>
 </nav>
 <?php endif ?>
